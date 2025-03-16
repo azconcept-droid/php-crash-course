1cd72d86eef2d7a85803e125baafbac6c4bb54c7
Yahaya Azeez <azconcept2016@gmail.com>
2025-03-16 23:12:42 +0100

    first commit

---
+++ 01_variables.php
@@ -0,0 +1,23 @@
+<?php
+
+// single line comment
+
+/** Varibales & types
+string
+
+*/
+
+$name = "zura";
+$age = 32;
+$isFather = true;
+$money = 19.99;
+$salary = null;
+
+echo $name;
+echo "Hello " . $name . "!<br>";
+echo "Hello {$name}!<br>";
+echo 'Hello {$name}!<br>';
+echo $age."<br>";
+echo $isFather."<br>";
+echo $money."<br>";
+echo $salary."<br>";
